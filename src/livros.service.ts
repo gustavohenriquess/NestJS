9ed792c62718f0830c2be75953e9790e1cdd41c6
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-02-19 20:29:31 -0300

    refactor: troca de Produtos para Livraria

---
+++ src/livros.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Livro } from './livro.model';
+
+@Injectable()
+export class LivrosService {
+  livros: Livro[] = [
+    new Livro('LIV01', 'Livro TDD e BDD na prática', 29.9),
+    new Livro('LIV02', 'Livro Iniciando com Flutter', 39.9),
+    new Livro('LIV03', 'Inteligência artificial como serviço', 39.9),
+  ];
+
+  obterTodos(): Livro[] {
+    return this.livros;
+  }
+
+  obterUm(id: string): Livro {
+    return this.livros[0];
+  }
+
+  criar(livro: Livro) {
+    this.livros.push(livro);
+  }
+
+  alterar(livro: Livro): Livro {
+    return livro;
+  }
+
+  apagar(id: string) {
+    this.livros.pop();
+  }
+}
