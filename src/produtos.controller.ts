091b143f6c46d9607691aa955ed002d80717aef0
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-02-19 17:26:40 -0300

    feat(produto): modelando produto

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -8,28 +8,35 @@ import {
   Put,
 } from '@nestjs/common';
 
+import { Produto } from './produto.model';
+
 @Controller('produtos')
 export class ProdutosController {
+  produtos: Produto[] = [
+    new Produto('LIV01', 'Livro TDD e BDD na prática', 29.9),
+    new Produto('LIV02', 'Livro Iniciando com Flutter', 39.9),
+    new Produto('LIV03', 'Inteligência artificial como serviço', 39.9),
+  ];
+
   @Get()
-  obterTodos(): string {
-    return 'Lista de produtos';
+  obterTodos(): Produto[] {
+    return this.produtos;
   }
 
   @Get(':id')
-  obterUm(@Param() params): string {
-    return `Produto com id ${params.id}`;
+  obterUm(@Param() params): Produto {
+    return this.produtos[0];
   }
 
   @Post()
-  criar(@Body() produto): string {
-    console.log(produto);
-    return 'Produto criado';
+  criar(@Body() produto) {
+    produto.id = 100;
+    this.produtos.push(produto);
   }
 
   @Put()
-  alterar(@Body() produto): string {
-    console.log(produto);
-    return `Produto atualizado`;
+  alterar(@Body() produto): Produto {
+    return produto;
   }
 
   @Delete(':id')
