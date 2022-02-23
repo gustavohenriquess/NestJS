43fc474b05ce639285ff7c148876bc73f92343fd
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-02-23 07:49:18 -0300

    feat(04): add tratamento de Erro, compatibilidade com Exprees ou fastify

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { FiltroDeExcecaoHttp } from './common/filtros/filtro-de-excexao-http.filter';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
@@ -7,6 +8,7 @@ import { UsuarioModule } from './usuario/usuario.module';
   controllers: [],
   providers: [
     { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    { provide: APP_FILTER, useClass: FiltroDeExcecaoHttp },
   ],
 })
 export class AppModule {}
